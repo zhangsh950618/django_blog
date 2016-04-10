66eba56f1426a86df49e29aab2c54af0c1cdb4b0
zhangshaohua <zhangsh950618@icloud.com>
2016-04-10 18:20:10 +0800

    change database

--- settings/base.py
+++ settings/base.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',
+        'USER': 'zsh',
+        'PASSWORD': '123',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
